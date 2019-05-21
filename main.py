5552dd2a1fe7ead8abd72efbced7738da4ec464a
Julie Norris <julienorris0715@gmail.com>
2019-05-21 13:53:31 -0700

    Update main.py
    Added step 3 - server returns response in json format

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+
+"""Step 3: create a route in your python server that takes a fullname argument, splits it into first and last name and returns a response in a json format, e.g.:
+"""
+
+@app.route("/user")
+def username_first_last():
+
+		username = request.args.get('username')
+		(firstname, lastname)= username.split(" ")
+		user = {
+			'firstname':firstname,
+			'lastname':lastname
+				}
+		return jsonify(user)
+		
 	
 
 if __name__ == "__main__":
